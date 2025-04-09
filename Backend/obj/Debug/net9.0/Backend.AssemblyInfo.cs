1a1252c24047095cbe77ba1d69767415a160c25d
alexandraa101 <150080324+alexmwondoka@users.noreply.github.com>
2025-04-09 21:04:53 +0200

    Modifications

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd5db8ecf0b29d00f9ab374e6f80be4e38adb2fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6823e32367c0f4a6b22c50d987e9b7109a85f1e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
